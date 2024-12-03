54601d03e1b7d278115a6a9a2df46ab7126870d9
Antoni <antoni.jakowicz@cyfroweszkoly.pl>
2024-12-03 22:50:48 +0100

    Aplikacja dzialajaca w koncu jezu

--- MovieTracker/app/src/main/java/com/example/movietracker/MainActivity.kt
+++ MovieTracker/app/src/main/java/com/example/movietracker/MainActivity.kt
@@ -1,23 +1,41 @@
 package com.example.movietracker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: MovieAdapter
+    private val movieList = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = MovieAdapter(movieList)
+        recyclerView.adapter = adapter
+        Log.d("MainActivity", "Adapter ustawiony: ${adapter}")
+
+
 
         var seekBar = findViewById<SeekBar>(R.id.seekBar)
         val myRatingTextView = findViewById<TextView>(R.id.myRatingTextView)
@@ -25,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         seekBar.progress = 0
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            @SuppressLint("SetTextI18n")
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 myRatingTextView.text = ("Twoja ocena: "+(progress+1).toString())
             }
@@ -34,7 +53,62 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
             }
-
         })
+
+
+        findViewById<Button>(R.id.addButton).setOnClickListener{
+            addMovie()
+        }
+
+        loadMovies()
+    }
+
+    private fun addMovie() {
+        val title = findViewById<EditText>(R.id.titleEditText).text.toString()
+        val genre = if(findViewById<RadioButton>(R.id.radioDrama).isChecked) "Dramat" else "Komedia"
+        val rating = findViewById<SeekBar>(R.id.seekBar).progress + 1
+        val opinion = findViewById<EditText>(R.id.opinionEditText).text.toString()
+
+        if (title.isNotEmpty()) {
+            val exists = movieList.any { it.title == title }
+            if (exists) {
+                Toast.makeText(this, "Film o tym tytule już istnieje!", Toast.LENGTH_SHORT).show()
+                Log.d("MainActivity", "Nie dodano filmu, ponieważ już istnieje: $title")
+            } else {
+                val newMovie = Movie(title, genre, rating, opinion)
+                movieList.add(newMovie)
+                Log.d("MainActivity", "Aktualna lista filmów: $movieList")
+                adapter.notifyDataSetChanged()
+                saveMovies()
+            }
+        } else {
+            Toast.makeText(this, "Wpisz tytuł", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveMovies() {
+        val gson = Gson()
+        val json = gson.toJson(movieList)
+        val file = File(filesDir, "movies.json")
+        file.writeText(json)
+
+        Log.d("MainActivity", "Zapisano filmy: $json")
+    }
+
+    private fun loadMovies(){
+        val file = File(filesDir, "movies.json")
+        if (file.exists()){
+            val gson = Gson()
+            val json = file.readText()
+            val type = object : TypeToken<List<Movie>>() {}.type
+            val loadedMovies: List<Movie> = gson.fromJson(json, type)
+            movieList.clear()
+            movieList.addAll(loadedMovies)
+            adapter.notifyDataSetChanged()
+            Log.d("MainActivity", "Załadowane filmy: $movieList")
+        }
+        else {
+            Log.d("MainActivity", "Plik movies.json nie istnieje. Lista jest pusta.")
+        }
     }
 }
